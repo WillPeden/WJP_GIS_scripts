97614c31125595f34df1bd900f9092994f7c25a8
William Peden <45796324+WillPeden@users.noreply.github.com>
2019-10-09 11:49:43 +0100

    Update plot_google_loc_data_download.R
    improving the inscript notes

--- plot_google_loc_data_download.R
+++ plot_google_loc_data_download.R
@@ -9,24 +9,25 @@ loc = x$locations
 # converting time column from posix milliseconds into a readable time scale
 loc$time = as.POSIXct(as.numeric(x$locations$timestampMs)/1000, origin = "1970-01-01")
 
-# converting longitude and latitude from E7 to GPS coordinates
+# converting longitude and latitude from E7 to GPS coordinates, save as vectors
 lat = loc$latitudeE7 / 1e7
 lon = loc$longitudeE7 / 1e7
 
-# calculate the number of data points per day, month and year
-loc$date <- as.Date(loc$time, format = '%Y/%m/%d')
+# extract dates as vector
 date <- as.Date(loc$time, format = '%Y/%m/%d')
 
+# combine vectors to create data frame for use with sf
 t <- data.frame(id = 1:length(date),date=date, lon = lon, lat = lat)
 locations = st_as_sf(t, coords = c("lon", "lat"), crs = 4326, agr = "constant", remove=F)
 
-#remove scary erroneous points in India(!)
+# remove scary erroneous points in India(!)
 locations <- locations[locations$lon < 70,]
 
-#create sf from 
+# load in some country polygons
 world <- ne_countries(scale = "large", returnclass = "sf")
 europe <- world[world$region_un=="Europe",]
 
+# all points plot
 all <- ggplot() +
   geom_sf(data=europe, fill= "antiquewhite") +
   geom_sf(data = locations, col = "red") +
@@ -35,6 +36,7 @@ all <- ggplot() +
   theme(panel.grid.major = element_line(color = gray(.5), linetype = "dashed", size = 0.5), 
         panel.background = element_rect(fill = "aliceblue"))
 
+# UK points plot (can change coord_sf to cover all your data)
 UK <- ggplot() +
   geom_sf(data=europe, fill= "antiquewhite") +
   geom_sf(data = locations, col = "red") +
@@ -43,6 +45,7 @@ UK <- ggplot() +
   theme(panel.grid.major = element_line(color = gray(.5), linetype = "dashed", size = 0.5), 
         panel.background = element_rect(fill = "aliceblue"))
 
+# The following just combines the two maps onto one plot
 p2 <- ggplot() +
   coord_equal(xlim = c(0, 2), ylim = c(0, 1.3), expand = FALSE) +
   annotation_custom(ggplotGrob(all), xmin = 0, xmax = 1, ymin = 0, 
@@ -51,7 +54,8 @@ p2 <- ggplot() +
                     ymax = 1.3) +
   theme_void()
 
+# save as a png
 ggsave("<YOUR MAP OUTPUT LOCATION HERE>.png", plot = p2, dpi=600, scale = 2.5)
 
-##can also creat interactive map with mapview
-#mapview::mapview(locations)
+# can also create an interactive map with mapview
+mapview::mapview(locations)
