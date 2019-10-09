7cdd016645831558c9b52622e4aead99367c08ba
William Peden <45796324+WillPeden@users.noreply.github.com>
2019-10-09 11:59:42 +0100

    Update Points_to_polygons.R
    generalised the script to be adapted for any data

--- Points_to_polygons.R
+++ Points_to_polygons.R
@@ -1,24 +1,28 @@
-##points to polygon script
+## points to polygon script
+
+# assumes points are in correct order for drawing polygon
 
 library(tidyverse)
 library(sf)
 
-temp <- read.csv("C:/Users/WP01/OneDrive - CEFAS/Temp/For_jemma/20190624 Scottish Dredge Areas Coords.csv")
-temp<- subset(temp, select = c("Licence", "Dredge.Area.Name", "Lat", "Long"))
+
+temp <- read.csv("<YOUR CSV FILE PATH HERE>.csv")
+# optional subset of data we require and removal of duplicates
+temp<- subset(temp, select = c("licence", "area_name", "lat", "long"))
 temp <- temp[!duplicated(temp),]
 
-polygon<-temp %>%
-  st_as_sf(coords = c("Long", "Lat"), crs = 4326) %>%
-  group_by(Dredge.Area.Name) %>%
+polygon <- temp %>%
+  st_as_sf(coords = c("long", "lat"), crs = 4326) %>%
+  group_by(area_name) %>%
   summarise(geometry = st_combine(geometry)) %>%
   st_cast("POLYGON") 
 
-#add licence number column
-polygon$Licence<-NA
+# add licence number column, or can add any other descriptive information as required
+polygon$licence <- NA
 for (i in 1:nrow(polygon)){
-  dr <- as.character(polygon[i,]$Dredge.Area.Name)
-  polygon[i,]$Licence <- as.character(temp[temp$Dredge.Area.Name==dr,]$Licence[1])
+  dr <- as.character(polygon[i,]$area_name)
+  polygon[i,]$licence <- as.character(temp[temp$area_name==dr,]$licence[1])
 }
 
-st_write(polygon, "C:/Users/WP01/OneDrive - CEFAS/Temp/For_jemma/Dredge_areas.shp", delete_layer=TRUE)
+st_write(polygon, "<YOUR OUTPUT PATH HERE>/polygon_name_here.shp", delete_layer=TRUE)
 
